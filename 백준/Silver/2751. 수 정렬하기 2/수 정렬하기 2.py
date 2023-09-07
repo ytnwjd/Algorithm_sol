14f16d6245a7f2cf51173183af15e8ceb1501875
y.tnwjd <104577505+ytnwjd@users.noreply.github.com>
2023-09-07 18:20:25 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1512 ms, Memory: 77108 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,12 @@
+import sys
+input= sys.stdin.readline
+cnt = int(input())
+li = []
+
+for i in range(cnt):
+    num = int(input())
+    li.append(num)
+
+li.sort()
+for j in li:
+    print(j, end=' ')
