b1ee46391daf5ed803d3a7cca6e27c680a6f09a1
y.tnwjd <104577505+ytnwjd@users.noreply.github.com>
2023-09-15 15:31:16 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    string = input()
+    chk = 0
+    stack = []
+
+    for i in string:
+        if(i == "("):
+            chk += 1
+        elif(i == ")"):
+            chk -= 1
+        
+        if(chk < 0):
+            break
+
+    if(chk == 0):
+        print("YES")
+    elif(chk < 0):
+        print("NO")
+    else:
+        print("NO")
