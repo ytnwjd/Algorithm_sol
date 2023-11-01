14d7ff617f6a3c84914b11298202f9e133203839
y.tnwjd <104577505+ytnwjd@users.noreply.github.com>
2023-11-01 20:22:13 +0900

    [Silver IV] Title: 개미, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3048. 개미/개미.py
+++ 백준/Silver/3048. 개미/개미.py
@@ -1,30 +1,13 @@
-import sys
-input = sys.stdin.readline
-
 n1, n2 = map(int, input().split())
-ant1 = list(input().strip("\n"))
-ant2 = list(input().strip("\n"))
-time = int(input())
+ant1 = list(input().strip())
+ant2 = list(input().strip())
+t = int(input().strip())
 
-ant_dic = dict()
-for a in ant1:
-    ant_dic[a] = 0
-for a in ant2:
-    ant_dic[a] = 1
+ant1.reverse()
+lastIndex = len(ant2)
+for i in range(min(t, n1)):
+    char = ant1.pop()
+    ant2.insert(min(lastIndex, t - i), char)
 
-ant1 = ant1[::-1]
-ant = ant1 + ant2
-
-tmp = ""
-for _ in range(time):
-    i = 0
-    while (i < len(ant)-1):
-        if(ant_dic[ant[i]]==0 and ant_dic[ant[i+1]]==1):
-            tmp = ant[i]
-            ant[i] = ant[i+1]
-            ant[i+1] = tmp
-            i += 1
-        i += 1
-
-for a in ant:
-    print(a, end="")
+answer = "".join(ant1 + ant2)
+print(answer)
