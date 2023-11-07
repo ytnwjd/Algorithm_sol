7a2c05f7ebac0ce02f5fbb802f4165a1350dfebf
y.tnwjd <104577505+ytnwjd@users.noreply.github.com>
2023-11-08 02:49:40 +0900

    [Silver IV] Title: 암기왕, Time: 1528 ms, Memory: 296676 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -0,0 +1,41 @@
+import sys
+import math
+input = sys.stdin.readline
+
+def binary_search(find_li, val):
+    i = 0
+    j = len(find_li)    #5
+
+    
+    mid = math.ceil((i + j)//2)
+    j -= 1
+    while True:
+    
+        # print("mid -", mid, "i -", i, "j -", j)
+        if(mid < 0 or mid >= len(find_li)):
+            return 0
+        if(i == mid or j == mid):
+            if(val == find_li[i] or val == find_li[j]):
+                return 1
+            else:
+                return 0
+
+        if (val == find_li[mid]):
+            return 1
+        elif (val < find_li[mid]):
+            j = mid
+        else:
+            i = mid
+
+        mid = math.ceil((i + j)//2)
+
+
+
+for _ in range(int(input())):
+    m = int(input())
+    note1 = sorted(list(map(int, input().strip().split())))
+    n = int(input())
+    note2 = list(map(int, input().strip().split()))
+
+    for j in range(n):
+        print(binary_search(note1, note2[j]))
