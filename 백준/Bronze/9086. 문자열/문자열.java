ecd2948288be15401a9266f3cddc5bae16bc6f58
y_tnwjd <ysj120808@gmail.com>
2025-01-15 15:04:38 +0900

    [Bronze V] Title: 문자열, Time: 188 ms, Memory: 18300 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String str = sc.next();
+            int len = str.length();
+
+            System.out.println(str.charAt(0)+""+str.charAt(len-1));
+        }
+
+    }
+}
