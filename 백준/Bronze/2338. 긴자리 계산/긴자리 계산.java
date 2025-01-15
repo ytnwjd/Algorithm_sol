90667e134484a2f90592ac05697a377cd3713651
y_tnwjd <ysj120808@gmail.com>
2025-01-15 10:46:09 +0900

    [Bronze V] Title: 긴자리 계산, Time: 120 ms, Memory: 15404 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BigInteger A = new BigInteger(br.readLine());
+        BigInteger B = new BigInteger(br.readLine());
+
+        System.out.println(A.add(B));
+        System.out.println(A.subtract(B));
+        System.out.println(A.multiply(B));
+    }
+}
